00ff353a1a67d474cd21b8a58d5ddc9766837a11
Frydrychowski <frydrychowskiv@gmail.com>
2022-05-18 15:52:08 +0200

    functionnal cnn model

--- bin/cnn.py
+++ bin/cnn.py
@@ -5,17 +5,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class CNN(nn.Module):
-    def __init__(self, nb_feature = 7, nb_sign = 9,hidden= 832, kWidth = 5, width = 600):
+    def __init__(self, nb_feature = 7, nb_sign = 9,hidden= 832, kWidth = 5, width = 600, nb_sample = 10):
         super(CNN, self).__init__()
         self.conv1 = nn.Conv2d(in_channels=1,out_channels=32,kernel_size=(nb_feature*nb_sign,kWidth))
         self.conv2 = nn.Conv2d(in_channels=32,out_channels=64,kernel_size=(1,2))
 
         self.pool = nn.MaxPool2d(1, 2)
 
-        conv1sizex , conv1sizey = (nb_feature*nb_sign, width-kWidth//2*2)
-        pool1sizex, pool1sizey = (conv1sizex, conv1sizey//2)
-        conv2sizex , conv2sizey = (pool1sizex, pool1sizey-2)
-        pool2sizex, pool2sizey = (conv2sizex, conv2sizey//2)
+        conv1sizex , conv1sizey = (1, width//nb_sample-kWidth//2*2)
+        pool1sizex, pool1sizey = (1, conv1sizey//2)
+        conv2sizex , conv2sizey = (1, pool1sizey-1)
+        pool2sizex, pool2sizey = (1, conv2sizey//2)
         self.pool2size = 64*(pool2sizex*pool2sizey)
 
         self.fc1 = nn.Linear(self.pool2size, hidden)
@@ -31,17 +31,19 @@ class CNN(nn.Module):
     def forward(self, x):
         
         #x = self.norm0(x)
-
+        print(x.shape)
         x = self.conv1(x)
+        print(x.shape)
         #x = self.normc1(x)
         x = nn.functional.relu(x)
         x = self.pool(x)
-
+        print(x.shape)
         x = self.conv2(x)
+        print(x.shape)
         #x = self.normc2(x)
         x = nn.functional.relu(x)
         x = self.pool(x)
-
+        print(x.shape)
         x = x.view(-1, self.pool2size) # flatten
 
         x = self.fc1(x)
@@ -54,6 +56,7 @@ class CNN(nn.Module):
 if __name__ == '__main__':
     import DOP
     import simulator
+    torch.set_default_dtype(torch.float64)
 
     data = simulator.nump[:,:300]
     dop = DOP.image(data, 10, 7)#issue in image builder (length = 1)
@@ -61,5 +64,7 @@ if __name__ == '__main__':
     plt.imshow(dop, interpolation='nearest')
     plt.savefig("im.png", dpi=1200)
     plt.show()
+    
     cnn = CNN(width=300)
+    print(cnn)
     print(cnn(dopTensor))
