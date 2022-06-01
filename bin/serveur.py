85b7c3dc72cbc4e545e544eb3e7d7c9acd4ac342
Frydrychowski <frydrychowskiv@gmail.com>
2022-06-01 14:50:04 +0200

    data passed by argument, not queu (win limitation)

--- bin/serveur.py
+++ bin/serveur.py
@@ -12,28 +12,13 @@ PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 
 class process:
     def __init__(self):
-        self.lock = Lock() #semaphore
         self.database = np.array([[0]]*9) #databased to be fill
-        self.dataAtt = Value('i', lock=False)
-        self.isReading = Value('i', lock=False)
-        self.attLecture = Condition(self.lock)
-        self.q = SimpleQueue()
 
-    def constrIm(self):
+    def constrIm(self, data):
         """
         fetch data and construct DOP
         """
-        with self.lock:
-            while(self.dataAtt.value == 0 or self.isReading.value > 0):
-                #someone is reading or there is no data waiting
-                self.attLecture.wait()
-            self.isReading.value += 1 #begin reading
-            data = self.q.get()
-            print(data)
-            print(data.shape)
-            self.dataAtt.value -= 1 #one data fetch
-            self.isReading.value -= 1 #end reading
-            self.attLecture.notify()
+
         start = time.time()
         im = DOP.image(data, 20, 7)
         end = time.time()
@@ -47,19 +32,14 @@ class process:
     def lect(self, taillIm, recouvrement):
         """
         process reading values from recup proccess
-        """       
-        #initiate proces number waiting for reading data in q
-        with self.lock:
-            self.dataAtt.value = 0
-            self.isReading.value = 0
-        p = process()
+        """
 
         #initiate tcp connection
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.bind((HOST, PORT))
             s.listen() #serveur listening to clints requests
-
-            for i in range(1469): #subjectiv numbre of connection allowed for this process
+            i = 0
+            while(1): #subjectiv numbre of connection allowed for this process
                 conn, addr = s.accept()
                 with conn:
                     print(f"Connected by {addr}")
@@ -79,17 +59,15 @@ class process:
                 if i%(taillIm//recouvrement) == 0 and i >= taillIm: 
                     print(">>>>launch dop construction")
                     #start the process of DOP creation
-                    Process(target=p.constrIm).start()
-                    #send requierd data through q
+                    
                     imData = np.copy(self.database[:,-taillIm:])
-                    with p.lock:
-                        #print(imData.shape)
-                        p.q.put(imData)
-                        p.dataAtt.value += 1
-                        print("send data")
-                        p.attLecture.notify()
+
+                    Process(target=p.constrIm, args=(imData,)).start()                    
+                   #print(imData.shape)
+                    print("send data")
+                i += 1
 
 
 if __name__ == '__main__':
     p = process()
-    p.lect(600, 4)
+    p.lect(200, 1)
