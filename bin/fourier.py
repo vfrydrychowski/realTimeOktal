4a98cd90a0430142c54bcc6d60e748df44101139
Frydrychowski <frydrychowskiv@gmail.com>
2022-06-08 10:30:50 +0200

    add fourier calculation

---
+++ bin/fourier.py
@@ -0,0 +1,20 @@
+import numpy as np
+from sklearn.manifold import spectral_embedding
+
+
+def spectrogramme(data: np.array) -> np.array:
+    """
+    create the 'spectrogram' of data, one fourier transformation for each signal
+    Args:
+        data : (nb_sample) np.array
+    """
+    spec = np.array([np.fft.fftshift(np.fft.fft(data[x,:])) for x in range(data.shape[0])])
+    return spec
+
+if __name__ == '__main__':
+    import clientTest
+    import matplotlib.pyplot as plt
+    data = clientTest.nump
+    plt.imshow(data[:,:50], interpolation='nearest', cmap='magma')
+    plt.savefig("im.png", dpi=1200)
+    plt.show()
